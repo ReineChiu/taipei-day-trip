ce504b8a22d27aac3a81ded34fb9c5a3acfc5dad
ReineChiu <jodie0729@gmail.com>
2022-12-07 19:03:27 +0800

    add photo transition effect

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -1,14 +1,18 @@
 
-const imgsContainer = document.querySelector(".img-container")
+const images = document.querySelector(".images")
 const nameCatMrt = document.querySelector(".name-catmrt");
 const intro = document.querySelector(".intro");
 const address = document.querySelector(".address");
 const direction = document.querySelector(".direction");
 
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+let index = 0; 
+let circle = document.querySelector(".circle");
+
 const morning = document.getElementById("morning");
 const afternoon = document.getElementById("afternoon");
 const price = document.querySelector(".price");
-
 morning.addEventListener("click",function(){
     price.textContent="2000元"
 });
@@ -16,9 +20,6 @@ afternoon.addEventListener("click",function(){
     price.textContent="2500元"
 });
 
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
-let index = 0;
 
 let path = window.location.pathname 
 let apiUrl = `/api`+ path;
@@ -28,56 +29,76 @@ fetch(apiUrl).then((response) =>{
 }).then((data) =>{
     const attraction = data["data"];
     const imgeUrls = attraction.images;
-    
-    for(img of imgeUrls){
-        let image = document.createElement("img");
-        image.setAttribute("class","image")
-        image.src = img
-        imgsContainer.appendChild(image);
-    }
-
-    let circle = document.querySelector(".circle");
-    let num = 0;
+    document.title = attraction.name;
+    for (let i=imgeUrls.length; i>0; i--){
+        let imageAll = document.createElement("img");
+        imageAll.style.zIndex = index;
+        index++;
+        imageAll.setAttribute("class","image")
+        imageAll.src = imgeUrls[i-1];
+        images.appendChild(imageAll);     
+    }   
+    // 圓點=張數 
     for (let i=0; i<imgeUrls.length; i++){
         let li = document.createElement("li");
         circle.appendChild(li)
-        //點原點切換圖片(可以不使用)
-        li.setAttribute("index", i)
-        li.addEventListener("click", function(){
-            for(let j=0; j<circle.children.length; j++){
-                circle.children[j].className = "";
-            }
-            this.className = "current";
-            num = this.getAttribute("index");
-            image.src = imgeUrls[num];
-        })
     }
-
-    let image = document.querySelector("img");
+    // 點Arrow，切換上下張
+    let image = document.querySelectorAll(".image");
     let circleList = document.querySelectorAll("li")
     circleList[0].className = "current"
-
-    leftArrow.addEventListener("click",function(){
-        index--;
-        if (index <0){
-            index = imgeUrls.length - 1;   
+    for (let i=0; i<imgeUrls.length; i++){
+        if (i != imgeUrls.length-1){
+            image[i].style.opacity = 0;
         }
-        image.src = imgeUrls[index];
+    }
+
+    let page = 0; 
+    let item = imgeUrls.length - 1; 
+    leftArrow.addEventListener("click",function(){
+        item++;
+        if (item >imgeUrls.length - 1){
+            item = 0;
+        }
+        for (i=0; i<imgeUrls.length; i++){
+            if (i !== item){
+                image[i].style.opacity = 0;    
+            }else{
+                image[item].style.transition="opacity 2s"; 
+                image[item].style.opacity = 1; 
+            }
+        }
+         //圓點隨圖片改動
+        page--;
+        if (page <0){
+            page = imgeUrls.length- 1;   
+        }
         for (let i=0; i<imgeUrls.length; i++){ 
-            circleList[i].className = "";          
-            circleList[index].className = "current";
-        }        
+        circleList[i].className = "";          
+        circleList[page].className = "current";
+        }
     })
     rightArrow.addEventListener("click",function(){
-        index++;
-        if (index >imgeUrls.length - 1){
-            index = 0;
+        item--;
+        if (item < 0){
+            item = imgeUrls.length-1;   
         }
-        image.src = imgeUrls[index];
+        for (i=imgeUrls.length-1; i>-1; i--){
+            if (i === item){
+                image[item].style.opacity = 1;
+                image[item].style.transition="opacity 2s";    
+            }else{
+                image[i].style.opacity = 0;
+            }
+        }
+        page++;
+        if (page>imgeUrls.length-1){
+            page = 0;
+        }   
         for (let i=0; i<imgeUrls.length; i++){
             circleList[i].className = "";
-            circleList[index].className = "current";   
-        }        
+            circleList[page].className = "current";   
+        }  
     })
     
     const name = document.createElement("div");
@@ -89,8 +110,7 @@ fetch(apiUrl).then((response) =>{
         cateAtMrt.textContent = attraction.category +" at " + attraction.mrt;
     }else{
         cateAtMrt.textContent = attraction.category +" （附近無相鄰捷運站）"
-    }
-    
+    }    
     nameCatMrt.appendChild(name);
     nameCatMrt.appendChild(cateAtMrt);
 
