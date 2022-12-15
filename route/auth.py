a2df8022f70bfdb57a41ce857b29d5c3a46a73af
ReineChiu <jodie0729@gmail.com>
2022-12-15 19:00:54 +0800

    p5 add bcrypt

--- route/auth.py
+++ route/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, make_response
-import os, json, jwt, datetime
+import os, json, jwt, datetime, bcrypt
 from dotenv import load_dotenv
 from mysql_connect import select_user, insert_user
 from utils.regex import verify_name, verify_email, verify_password
@@ -25,16 +25,21 @@ def singup():
     register_user = select_user(email = email)
     if register_user:
         return ({"error":True, "message":"信箱已被註冊"},400)
-    try:
-        insert_user(username = username, email = email, password = password)
-        confirmuser = select_user(email = email, password = password)
-        if confirmuser:
-            return ({"ok":True},200)
-        else:
-            return ({"error":True, "message":"信箱已被使用"},400)
-    except Exception as e:
-        print(f"{e}:註冊過程失敗")
-        return ({"error":True, "message":"註冊過程發生錯誤"},500)
+    if not register_user:
+        try:
+            # 對密碼做hash
+            en_pwd = password.encode('utf-8')
+            hashed = bcrypt.hashpw(en_pwd, bcrypt.gensalt())
+            hashed_decode = bcrypt.hashpw(en_pwd, bcrypt.gensalt()).decode('utf-8')
+            insert_user (username = username, email = email, password = hashed_decode) # 將hash完的password.decode,存進資料庫
+            confirm_user = select_user(email = email, password = hashed_decode)
+            if confirm_user:
+                return ({"ok":True},200)
+            else:
+                return ({"error":True, "message":"信箱已被使用"},400)
+        except Exception as e:
+            print(f"{e}:註冊過程失敗")
+            return ({"error":True, "message":"註冊過程發生錯誤"},500)
 
 @api_auth.route("/user/auth", methods=["GET"])
 def check_user():
@@ -45,8 +50,7 @@ def check_user():
             data.pop("exp")
             return ({"data":data},200)
         else:
-            data = None
-            return ({"data":data},200)
+            return ({"data":None},200)
     except Exception as e:
         print(f"{e}:確認資訊發生錯誤")
         return ({"error":True, "message":"確認身份過程發生錯誤"},500)
@@ -62,20 +66,25 @@ def loginin():
     passwordRegex = verify_password(password = password)
     if not emailRegex or not passwordRegex:
         return ({"error":True, "message": "信箱、密碼輸入資料格式錯誤"},400)
+    
+    user = select_user(email = email)
     try:
-        user = select_user(email = email, password = password)
         if user:
-            token = jwt.encode({"id":user["id"],
-                                "username":user["username"],
-                                "email":user["email"], 
-                                "exp":datetime.datetime.utcnow()+datetime.timedelta(days=7)},
-                                os.getenv("JWT_SERECT_KEY"))
-            response = make_response({"ok":True},200)
-            response.set_cookie("token",token)
-
-            return (response)
+            en_pwd = password.encode('utf-8')
+            hashed = user["password"].encode('utf-8')# 跟資料庫密碼比對依據
+            if bcrypt.checkpw(en_pwd, hashed):
+                token = jwt.encode({"id":user["id"],
+                                    "username":user["username"],
+                                    "email":user["email"], 
+                                    "exp":datetime.datetime.utcnow()+datetime.timedelta(days=7)},
+                                    os.getenv("JWT_SERECT_KEY"))
+                response = make_response({"ok":True},200)
+                response.set_cookie("token",token)
+                return (response)
+            else:
+                return ({"error":True, "message":"密碼輸入錯誤"},400)
         else:
-            return ({"error":True, "message":"登入資料輸入錯誤"},400)
+            return ({"error":True, "message":"帳號或密碼輸入錯誤"},400)
     except Exception as e:
         print(f"{e}:登入過程失敗")
         return ({"error":True, "message":"登入過程發生錯誤"},500)
